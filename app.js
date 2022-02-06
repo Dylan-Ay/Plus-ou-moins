719b4cac585066ce561c8f02fa90b4034ba247ba
Dylan <97350533+Dylan-Ay@users.noreply.github.com>
2022-02-06 18:46:14 +0100

    Update CSS, update du JavaScript

--- app.js
+++ app.js
@@ -1,32 +1,66 @@
 //Déclarations variables
-const computerScore = document.getElementById('computer-score');
-const playerScore = document.getElementById('player-score');
-const info = document.getElementById('info-game');
-const attempt = document.getElementById('attempt');
-const button = document.getElementById('button');
-const input = document.getElementById('inputNum');
+let computerScore = document.getElementById('computer-score');
+let playerScore = document.getElementById('player-score');
+let info = document.getElementById('info-game');
+let attempt = document.getElementById('attempt');
+let guessSubmit = document.querySelector('.guessSubmit'); //Valider
+let guessInput = document.getElementById('inputNum'); // Champ
+let attemptLeft = document.querySelector('.attempt-left');
+let counter = 10;
+let resetButton;
 
-input.addEventListener("keypress", function (event) {
-    if (event.target == 13) {
-      button.click();
+// Cliquer sur le boutton si entrée est actionné
+guessInput.addEventListener("keypress", function (event) {
+    if (event.keyCode == 13) {
+      guessSubmit.click();
     }
   });
 
-// Obtenir le nombre qui a été inseré
-let number = document.getElementById("inputNum").value;
 // Générer un nombre aléatoire
-let randomNumber = Math.floor(Math.random() * 100);
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
 
-// Afficher différentes informations en fonction du nombre inséré par l'utilisateur
-if (number === 100){
-    info.innerHTML = "Bravo vous avez trouvé le bon nombre !";
-} 
+// Vérification du nombre inséré et comparaison avec le randomNumber
+function checkGuess() {
+  let userGuess = Number(inputNum.value);
 
+  if (userGuess === randomNumber){
+    info.innerHTML = "Bravo, vous avez trouvé le bon nombre !";
+    playerScore.innerHTML++;
+    attemptLeft.style.display = "none";
+    setGameOver();
+  } else if (counter === 0){
+    info.innerHTML = "C'est Perdu !";
+    computerScore.innerHTML++;
+    attemptLeft.style.display = "none";
+    setGameOver();
+  } else {
+    if (userGuess < randomNumber){
+      info.innerHTML = "C'est Plus !";
+    } else if (userGuess > randomNumber){
+      info.innerHTML = "C'est Moins !";
+    }
+  }
+  guessInput.value = '';
+  guessInput.focus();
+}
 
-//info.innerHTML = "Salut";
-//console.log(getInputValue());
-//console.log(randomNumber);
+// A chaque clique sur "Valider" décrémenter le compteur de tentatives
+guessSubmit.addEventListener("click",()=>{
+  counter--;
+  attempt.innerHTML = counter;
+});
 
+// A chaque clique, exécuter la fonction checkGuess
+guessSubmit.addEventListener('click', checkGuess);
 
+// 
 
-
+function setGameOver() {
+  guessInput.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.innerHTML = 'Rejouer';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
