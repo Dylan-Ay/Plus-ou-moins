91b2f7c1fa6593db932b327f6d0a9d75bbfc3722
Dylan-Ay <dylan.ayache.68@gmail.com>
2022-02-07 21:15:02 +0100

    JavaScript fini, update règles CSS. Reste à modifier quelques petites choses. Uploaded

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-//Déclarations variables
+//Déclarations variables globales
 let computerScore = document.getElementById('computer-score');
 let playerScore = document.getElementById('player-score');
 let info = document.getElementById('info-game');
 let attempt = document.getElementById('attempt');
 let guessSubmit = document.querySelector('.guessSubmit'); //Valider
-let guessInput = document.getElementById('inputNum'); // Champ
+let guessInput = document.getElementById('inputNum'); // Champ d'insertion
 let attemptLeft = document.querySelector('.attempt-left');
-let counter = 10;
+let randomNumber = Math.floor(Math.random() * 100) + 1; // Génerer un nombre aléatoire 
 let resetButton;
+let replayInfo;
+let attemptDiv;
+let counter = 10;
 
 // Cliquer sur le boutton si entrée est actionné
 guessInput.addEventListener("keypress", function (event) {
@@ -16,9 +19,11 @@ guessInput.addEventListener("keypress", function (event) {
     }
   });
 
-// Générer un nombre aléatoire
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+// Création de la fonction pour générer un nouveau nombre aléatoire à chaque resetGame()
+function defineRandomNumber(){
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  return randomNumber;
+}
 
 // Vérification du nombre inséré et comparaison avec le randomNumber
 function checkGuess() {
@@ -54,13 +59,34 @@ guessSubmit.addEventListener("click",()=>{
 // A chaque clique, exécuter la fonction checkGuess
 guessSubmit.addEventListener('click', checkGuess);
 
-// 
 
+// Défini les paramètres à prendre en compte quand la partie est perdue
 function setGameOver() {
   guessInput.disabled = true;
-  guessSubmit.disabled = true;
+  guessSubmit.style.display = "none";
   resetButton = document.createElement('button');
+  let divButton = document.getElementById('insert-number'); // Récupère la div du bouton
+  divButton.appendChild(resetButton);
+  replayInfo = document.createElement('span')
+  attemptDiv = document.getElementById('attempt-div');
+  attemptDiv.appendChild(replayInfo);
+  replayInfo.innerHTML = 'Pour continuer la partie, veuillez cliquer sur "Rejouer".';
+  replayInfo.classList.add('replay-info');
   resetButton.innerHTML = 'Rejouer';
-  document.body.appendChild(resetButton);
   resetButton.addEventListener('click', resetGame);
+}
+
+// Relance une nouvelle partie
+function resetGame(){
+  resetButton.style.display = "none";
+  replayInfo.style.display = "none";
+  guessInput.disabled = false;
+  guessSubmit.disabled = false;
+  guessSubmit.style.display = "block";
+  guessInput.focus();
+  info.innerHTML = "-";
+  attemptLeft.style.display = "block";
+  counter = 10;
+  attempt.innerHTML = counter;
+  defineRandomNumber();
 }
