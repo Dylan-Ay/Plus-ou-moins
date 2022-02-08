37d2ebe8cdad82149b4526773d1f7761f7be1859
Dylan-Ay <dylan.ayache.68@gmail.com>
2022-02-08 18:04:22 +0100

    Ajouts restrictions, emojis, update CSS, update JS, update HTML,

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let guessSubmit = document.querySelector('.guessSubmit'); //Valider
 let guessInput = document.getElementById('inputNum'); // Champ d'insertion
 let attemptLeft = document.querySelector('.attempt-left');
 let randomNumber = Math.floor(Math.random() * 100) + 1; // Génerer un nombre aléatoire 
+let labelNumber = document.getElementById('label-number');
+let emoji = document.getElementById('emoji');
+let restriction = document.getElementById('tooltip');
 let resetButton;
 let replayInfo;
 let attemptDiv;
@@ -25,45 +28,58 @@ function defineRandomNumber(){
   return randomNumber;
 }
 
-// Vérification du nombre inséré et comparaison avec le randomNumber
+// Fonction principale du jeu
 function checkGuess() {
   let userGuess = Number(inputNum.value);
-
-  if (userGuess === randomNumber){
-    info.innerHTML = "Bravo, vous avez trouvé le bon nombre !";
-    playerScore.innerHTML++;
-    attemptLeft.style.display = "none";
-    setGameOver();
-  } else if (counter === 0){
-    info.innerHTML = "C'est Perdu !";
-    computerScore.innerHTML++;
-    attemptLeft.style.display = "none";
-    setGameOver();
-  } else {
-    if (userGuess < randomNumber){
-      info.innerHTML = "C'est Plus !";
-    } else if (userGuess > randomNumber){
-      info.innerHTML = "C'est Moins !";
-    }
+// Restrictions si vide ou si supérieur à 100 ou si inférieur à 1
+  if (userGuess == "" || userGuess > 100 || userGuess < 1){
+    restriction.style.display = "flex";
   }
-  guessInput.value = '';
-  guessInput.focus();
+// Si condition rempli afficher "c'est plus" et décrémenter le counter
+  else if (userGuess < randomNumber && userGuess > 0){
+    restriction.style.display = "none";
+    info.innerHTML = "C'est Plus !";
+    counter--;
+    attempt.innerHTML = counter;
+// Si condition rempli afficher "c'est moins" et décrémenter le counter
+  } else if (userGuess > randomNumber && userGuess <= 100){
+    restriction.style.display = "none";
+    info.innerHTML = "C'est Moins !";
+    counter--;
+    attempt.innerHTML = counter;
+// Si condition rempli changer l'émoticone
+  } if (counter == 7){
+    emoji.innerHTML = "&#128517";
+  } else if (counter == 4){
+        emoji.innerHTML = "&#128531";
+  } else if (counter == 2){
+        emoji.innerHTML = "&#128560";
+// Si condition rempli afficher un message et changer le score
+      } else {
+        if (userGuess === randomNumber){
+          info.innerHTML = "Bravo, vous avez trouvé le bon nombre, vous gagnez 1 point !";
+          playerScore.innerHTML++;
+          attemptLeft.style.display = "none";
+          setGameOver();
+        } else if (counter === 0){
+          info.innerHTML = "C'est Perdu, l'ordinateur gagne 1 point !";
+          computerScore.innerHTML++;
+          attemptLeft.style.display = "none";
+          setGameOver();
+        } 
+      }
+    guessInput.value = '';
+    guessInput.focus();
 }
-
-// A chaque clique sur "Valider" décrémenter le compteur de tentatives
-guessSubmit.addEventListener("click",()=>{
-  counter--;
-  attempt.innerHTML = counter;
-});
 
 // A chaque clique, exécuter la fonction checkGuess
 guessSubmit.addEventListener('click', checkGuess);
-
 
 // Défini les paramètres à prendre en compte quand la partie est perdue
 function setGameOver() {
   guessInput.disabled = true;
   guessSubmit.style.display = "none";
+  labelNumber.style.display = "none";
   resetButton = document.createElement('button');
   let divButton = document.getElementById('insert-number'); // Récupère la div du bouton
   divButton.appendChild(resetButton);
@@ -80,13 +96,15 @@ function setGameOver() {
 function resetGame(){
   resetButton.style.display = "none";
   replayInfo.style.display = "none";
+  labelNumber.style.display = "block";
   guessInput.disabled = false;
   guessSubmit.disabled = false;
   guessSubmit.style.display = "block";
   guessInput.focus();
-  info.innerHTML = "-";
+  info.innerHTML = "";
   attemptLeft.style.display = "block";
   counter = 10;
   attempt.innerHTML = counter;
+  emoji.innerHTML = "&#128512";
   defineRandomNumber();
 }
